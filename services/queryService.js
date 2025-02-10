857c7099651c188ab9ef313a57e4526687892dfd
priyanshibansal663 <priyanshibansal663@gmail.com>
2025-02-10 13:30:04 +0530

    user

--- services/queryService.js
+++ services/queryService.js
@@ -14,10 +14,15 @@ const sendEmail = async ({ fullName, email, number, message }) => {
     from: process.env.EMAIL_USER,
     to: process.env.WEBSITE_EMAIL,
     subject: "New Form Submission",
-    text: `Name: ${fullName}\nEmail: ${email}\nPhone: ${number}\nMessage: ${message || "No message provided"}`
+    text: `Name: ${fullName}\nEmail: ${email}\nPhone: ${number}\nMessage: ${message || "No message provided"}`,
   };
 
-  await transporter.sendMail(mailOptions);
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
 
 module.exports = sendEmail;
