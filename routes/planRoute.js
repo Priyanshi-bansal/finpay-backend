de5f41446d186f12b1e9ed0216003c7f7324d982
priyanshibansal663 <priyanshibansal663@gmail.com>
2025-02-13 12:56:39 +0530

    recharge APi

--- routes/planRoute.js
+++ routes/planRoute.js
@@ -5,7 +5,7 @@ const {getPlanTypes} = require('../controllers/planController')
 const router = express.Router();
 
 
-router.get('/plansAPI', getPlans);
-router.get('/plansTypeAPI', getPlanTypes);
+router.post('/plansAPI', getPlans);
+router.post('/plansTypeAPI', getPlanTypes);
 
 module.exports = router;
