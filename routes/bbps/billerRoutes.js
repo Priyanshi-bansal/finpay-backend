a50ddffb669f53285476f089d5efe4e2a6fad4ae
priyanshibansal663 <priyanshibansal663@gmail.com>
2025-02-26 15:23:35 +0530

    fgvhb

--- routes/bbps/billerRoutes.js
+++ routes/bbps/billerRoutes.js
@@ -5,6 +5,6 @@ const {billerInfo, billFetch} = require("../../controllers/bbps/billerController
 const router = express.Router();
 
 router.post("/biller-info", billerInfo);
-router.post("bill-fetch",billFetch);
+router.post("/bill-fetch",billFetch);
 
 module.exports = router;
